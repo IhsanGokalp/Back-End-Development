018e7dff10e2e3af5307ab0f6f9f72c177507099
IhsanGokalp <ihsangokalp01@hotmail.com>
2020-03-06 11:13:41 +0600

    Spr. Bt. In Action ch 3 is completed

---
+++ readingList/src/main/java/com/example/readinglist/AmazonProperties.java
@@ -0,0 +1,19 @@
+package com.example.readinglist;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("amazon")
+public class AmazonProperties {
+
+    private String associateID;
+
+    public String getAssociateID() {
+        return associateID;
+    }
+
+    public void setAssociateID(String associateID) {
+        this.associateID = associateID;
+    }
+}
--- readingList/src/main/java/com/example/readinglist/ReadingListController.java
+++ readingList/src/main/java/com/example/readinglist/ReadingListController.java
@@ -1,9 +1,8 @@
 package com.example.readinglist;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import java.util.List;
@@ -12,26 +11,31 @@ import java.util.List;
 @RequestMapping("/")
 public class ReadingListController {
 
+    private AmazonProperties amazonProperties;
     private ReadingListRepository readingListRepository;
 
-    @Autowired
-    public ReadingListController(ReadingListRepository readingListRepository) {
+    public ReadingListController(ReadingListRepository readingListRepository,
+                                 AmazonProperties amazonProperties) {
         this.readingListRepository = readingListRepository;
+        this.amazonProperties = amazonProperties;
     }
 
-    @RequestMapping(value="/{reader}", method=RequestMethod.GET)
-    public String readersBooks(@PathVariable("reader") String reader,Model model) {
+
+    @RequestMapping(method=RequestMethod.GET)
+    public String readersBooks(Reader reader,Model model) {
         List<Book> readingList =readingListRepository.findByReader(reader);
         if (readingList != null) {
             model.addAttribute("books", readingList);
+            model.addAttribute("reader", reader);
+            model.addAttribute("amazonID", amazonProperties.getAssociateID());
         }
         return "readingList";
     }
 
-    @RequestMapping(value="/{reader}", method=RequestMethod.POST)
-    public String addToReadingList(@PathVariable("reader") String reader, Book book) {
+    @RequestMapping(method=RequestMethod.POST)
+    public String addToReadingList(Reader reader, Book book) {
         book.setReader(reader);
         readingListRepository.save(book);
-        return "redirect:/{reader}";
+        return "redirect:/";
     }
 }
