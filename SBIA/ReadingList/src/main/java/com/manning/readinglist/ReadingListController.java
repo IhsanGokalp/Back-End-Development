b25ff409adb7842a7c816838fd8663886b3ab41c
IhsanGokalp <ihsangokalp01@hotmail.com>
2020-03-09 12:41:45 +0600

    Ch. 1 is completed

--- SBIA/ReadingList/src/main/java/com/manning/readinglist/ReadingListController.java
+++ SBIA/ReadingList/src/main/java/com/manning/readinglist/ReadingListController.java
@@ -1,40 +1,41 @@
 package com.manning.readinglist;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+
 
 import java.util.List;
-
 @Controller
 @RequestMapping("/")
 public class ReadingListController {
-
     private ReadingListRepository readingListRepository;
-
     @Autowired
-    public ReadingListController(ReadingListRepository readingListRepository){
+    public ReadingListController(
+            ReadingListRepository readingListRepository) {
         this.readingListRepository = readingListRepository;
     }
-
-    @RequestMapping(value = "/{reader}", method = RequestMethod.GET)
-    public String readerBooks(@PathVariable("reader") String reader, Model model) {
-        List<Book> books = readingListRepository.findByReader(reader);
-        if (books != null) {
-            model.addAttribute("books", books);
+    @RequestMapping(value="/{reader}", method= RequestMethod.GET)
+    public String readersBooks(
+            @PathVariable("reader") String reader,
+            Model model) {
+        List<Book> readingList =
+                readingListRepository.findByReader(reader);
+        if (readingList != null) {
+            model.addAttribute("books", readingList);
         }
-
         return "readingList";
     }
 
-    @RequestMapping(value = "/{reader}", method = RequestMethod.POST)
-    public String addToReadingList(@PathVariable("reader") String reader, Book book) {
+    @RequestMapping(value="/{reader}", method=RequestMethod.POST)
+    public String addToReadingList(
+            @PathVariable("reader") String reader, Book book) {
         book.setReader(reader);
+        System.out.println(book);
         readingListRepository.save(book);
         return "redirect:/{reader}";
     }
-    
 }
