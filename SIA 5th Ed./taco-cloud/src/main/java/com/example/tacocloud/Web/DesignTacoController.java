42bc696fd6315837c0be6d3694c079b5bbcb4e1c
IhsanGokalp <ihsangokalp01@hotmail.com>
2020-03-10 09:38:02 +0600

    Ch 2 done, some errors exsist

--- SIA 5th Ed./taco-cloud/src/main/java/com/example/tacocloud/Web/DesignTacoController.java
+++ SIA 5th Ed./taco-cloud/src/main/java/com/example/tacocloud/Web/DesignTacoController.java
@@ -1,14 +1,19 @@
 package com.example.tacocloud.Web;
 
 import com.example.tacocloud.Ingredient;
+import com.example.tacocloud.Taco;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @Slf4j
@@ -36,5 +41,19 @@ public class DesignTacoController {
 
         model.addAttribute("design",  new Taco());
         return "design";
+    }
+
+    @PostMapping
+    public String processDesign(@Valid Taco design, Errors errors){
+        if (errors.hasErrors())
+            return "design";
+        log.info("Processing design: " + design);
+        return "redirect:/orders/current";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Ingredient.Type type){
+        return ingredients.stream()
+                .filter(x -> x.getType().equals(type))
+                .collect(Collectors.toList());
     }
 }
